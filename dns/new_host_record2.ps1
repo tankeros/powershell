6145a204623f2e87b1bc2465ef232c5fff042b29
tankeros <54300108+tankeros@users.noreply.github.com>
2019-09-16 22:57:20 -0400

    Delete new_host_record2.ps1

--- dns/new_host_record2.ps1
+++
@@ -1,5 +0,0 @@
-#creating a new dns record with PTR (Reverse Lookup Zone mst be enable)
-dnscmd dc1 /recordadd jcloud.com ws3 /createptr A 192.168.1.145
-
-#creating a new dns record without PTR 
-dnscmd dc1 /recordadd jcloud.com ws5 A 192.168.1.147
